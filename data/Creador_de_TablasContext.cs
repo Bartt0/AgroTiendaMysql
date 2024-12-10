64e9990ef2a09888f6a8b4782622eb7fd5943732
Eduardo Cruz Alejo <ec3692654@gmail.com>
2024-12-10 09:28:28 -0600

    Realcion de casi todas las tablas

--- data/Creador_de_TablasContext.cs
+++ data/Creador_de_TablasContext.cs
@@ -41,6 +41,62 @@ namespace Agrotienda_2.data
             .WithMany(g => g.Producto)
             .HasForeignKey(s => s.UsuarioId)
             .OnDelete(DeleteBehavior.Cascade);
+        
+          //Relacion foreingkey de Pasarela_Pago y Venta//
+          modelBuilder.Entity<Pasarela_Pago>()
+            .HasOne<Venta>(s => s.Venta)
+            .WithMany(g => g.Pasarela_Pago)
+            .HasForeignKey(s => s.VentaId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            //Relacion foreingkey de Detalle_Venta y Venta//
+            modelBuilder.Entity<Detalle_Venta>()
+            .HasOne<Venta>(s => s.Venta)
+            .WithMany(g => g.Detalle_Venta)
+            .HasForeignKey(s => s.VentaId)
+            .OnDelete(DeleteBehavior.Cascade);
+           //Relacion foreingkey de Detalle_Venta y Producto//
+            modelBuilder.Entity<Detalle_Venta>()
+            .HasOne<Producto>(s => s.Producto)
+            .WithMany(g => g.Detalle_Venta)
+            .HasForeignKey(s => s.ProductoId)
+            .OnDelete(DeleteBehavior.Cascade);
+             //Relacion foreingkey de Detalle_Carrito y Carrito//
+            modelBuilder.Entity<Detalle_Carrito>()
+            .HasOne<Carrito>(s => s.Carrito)
+            .WithMany(g => g.Detalle_Carrito)
+            .HasForeignKey(s => s.CarritoId)
+            .OnDelete(DeleteBehavior.Cascade);
+            //Relacion foreingkey de Detalle_Carrito y producto//
+            modelBuilder.Entity<Detalle_Carrito>()
+            .HasOne<Producto>(s => s.Producto)
+            .WithMany(g => g.Detalle_Carrito)
+            .HasForeignKey(s => s.ProductoId)
+            .OnDelete(DeleteBehavior.Cascade);
+            //Relacion foreingkey de Chat y Usuario//
+             modelBuilder.Entity<Chat>()
+            .HasOne<Usuario>(s => s.Usuario)
+            .WithMany(g => g.Chat)
+            .HasForeignKey(s => s.UsuarioId)
+            .OnDelete(DeleteBehavior.Cascade);
+           //Relacion foreingkey de Carrito y Usuario//
+             modelBuilder.Entity<Carrito>()
+            .HasOne<Usuario>(s => s.Usuario)
+            .WithMany(g => g.Carrito)
+            .HasForeignKey(s => s.UsuarioId)
+            .OnDelete(DeleteBehavior.Cascade);
+            //Relacion foreingkey de Calificacion y Usuario//
+             modelBuilder.Entity<Calificacion>()
+            .HasOne<Usuario>(s => s.Usuario)
+            .WithMany(g => g.Calificacion)
+            .HasForeignKey(s => s.UsuarioId)
+            .OnDelete(DeleteBehavior.Cascade);
+            //Relacion foreingkey de Calificacion y Producto//
+            modelBuilder.Entity<Calificacion>()
+            .HasOne<Producto>(s => s.Producto)
+            .WithMany(g => g.Calificacion)
+            .HasForeignKey(s => s.ProductoId)
+            .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
